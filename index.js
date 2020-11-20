05a7f28daee658f717c1562c1424f37f8dacabf6
Patrik Visi <justvisitf@gmail.com>
2020-11-20 18:48:39 +0100

    working register and login, no ajax, ejs

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const queries=require('./lib/database/functions');
 const express=require('express');
 const path=require("path");
 const bodyp=require('body-parser');
 const {router}=require('./lib/routing');
 const functions = require('./lib/database/functions');
+const ejs=require('ejs');
+const { hashPass } = require('./lib/database/functions');
 
 const app=express();
 const port=process.env.PORT || 3000;
-console.log(functions.encrypt("alma"));
 app.use(express.static(path.join(__dirname,'lib/web/public/')));
 app.use(bodyp.urlencoded({extended:true}));
 app.use(bodyp.json());
 app.use(router);
-console.log(path.join(__dirname,'lib/web/public'));
-/*queries.addWeight("d2713cb3-e2dc-4586-9a15-9c0051473a38",69,"2020-11-05").then((result)=>{
-    console.log(result);
-});*/
+
 console.log(__dirname);
+
+console.log(functions.generateId());
 app.listen(port,()=>{
     console.log(`app is listening on ${port}`);
 });
